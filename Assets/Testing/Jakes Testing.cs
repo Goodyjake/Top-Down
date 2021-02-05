0ce04d97a3ac32c953e5d48c12df8d4016963fec
Goodyjake <yourguitarfriend@yahoo.com>
2021-02-05 13:40:22 -0600

    Added testing folder and Test script

---
+++ Assets/Testing/Jakes Testing.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JakesTesting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Welcome!!!
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

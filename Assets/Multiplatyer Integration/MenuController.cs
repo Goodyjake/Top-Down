0ca8148f00d403d9ead1cf272f0f8bfcdf0f8e72
Goodyjake <yourguitarfriend@yahoo.com>
2021-02-05 17:28:08 -0600

    Multiplayer Added

--- Assets/Multiplatyer Integration/MenuController.cs
+++ Assets/Multiplatyer Integration/MenuController.cs
@@ -1,18 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private string VersionName = "0.1";
+    [SerializeField] private GameObject UsernameMenu;
+    [SerializeField] private GameObject ConnectPanel;
+
+    [SerializeField] private InputField UsernameInput;
+    [SerializeField] private InputField CreateGameInput;
+    [SerializeField] private InputField JoinGameInput;
+
+    [SerializeField] private GameObject StartButton;
+
+
+
+    private void Awake()
     {
-        
+        PhotonNetwork.ConnectUsingSettings(VersionName);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        UsernameMenu.SetActive(true);
+    }
+
+    private void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+        Debug.Log("Connected");
+    }
+
+
+    public void ChangeUserNameInput()
+    {
+        if(UsernameInput.text.Length >= 3)
+        {
+            StartButton.SetActive(true);
+        }
+        else
+        {
+            StartButton.SetActive(false);
+        }
+    }
+  
+
+    public void SetUserName()
+    {
+        UsernameMenu.SetActive(false);
+        PhotonNetwork.playerName = UsernameInput.text;
+    }
+
+    public void CreateGame()
+    {
+        PhotonNetwork.CreateRoom(CreateGameInput.text, new RoomOptions() { maxPlayers = 8 }, null);
+           
+    }
+
+    public void JoinGame()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.maxPlayers = 8;
+        PhotonNetwork.JoinOrCreateRoom(JoinGameInput.text, roomOptions, TypedLobby.Default);
+    }
+
+
+    private void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("MainGame");
     }
 }

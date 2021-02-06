5fff5331f543548ab2665a1819f2ee0607d4cf97
Goodyjake <jake@scrubsgaming.com>
2021-02-06 15:58:26 -0600

    pt-Multiplayer2.0

---
+++ Assets/Scripts/GameManger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManger : MonoBehaviour
+{
+
+    public GameObject PlayerPrefab;
+    public GameObject GameCanvas;
+    public GameObject SceneCamera;
+
+    private void Awake()
+    {
+        GameCanvas.SetActive(true);
+    }
+
+    public void SpawnPlayer()
+    {
+        float RandomValue = Random.Range(-1f, 1f);
+
+        PhotonNetwork.Instantiate(PlayerPrefab.name, new Vector2(this.transform.position.x * RandomValue, this.transform.position.y), Quaternion.identity, 0);
+        GameCanvas.SetActive(false);
+        SceneCamera.SetActive(false);
+    }
+
+}
